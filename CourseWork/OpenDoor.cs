1db19a9f5bc9c05e1c87bc69d6150ecf59b96857
Sergey Zozulya <zozulia.sergey2011@yandex.ru>
2021-04-18 23:54:40 +0300

    Add files via upload
    Practicing

---
+++ CourseWork/OpenDoor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Vector3 defaultRot, openRot;
+    bool open;
+    bool enter;
+    public float DoorOpenAngle = 90.0f;
+    public float smooth = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultRot = transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            //Open door
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
+        }
+        else
+        {
+            //Close door
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+        }
+
+        if (Input.GetKeyDown("f") && enter)
+        {
+            open = !open;
+        }
+
+    }
+
+    void OnGUI()
+    {
+        if (enter)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height - 100, 150, 30), "Press 'F' to open the door");
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = false;
+        }
+    }
+
+
+}
